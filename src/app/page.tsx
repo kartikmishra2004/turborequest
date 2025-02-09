bf58e978b55571b3282d0a5229c041dc5ff1c537
kartikmishra2004 <kartik26nov2004@gmail.com>
2025-02-09 16:47:30 +0530

    Shadcn configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <main>
+    <main className="flex justify-center items-center gap-2 flex-col max-w-xl mx-auto min-h-screen">
       Hello!!
+      <Button variant="default">Button</Button>
     </main>
   );
 }
