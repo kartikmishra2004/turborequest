08692343c6b00228d8266aebfd5dbb217dcc8596
kartikmishra2004 <kartik26nov2004@gmail.com>
2025-02-11 02:17:16 +0530

    Fix error

--- src/app/components/CodeBlock.tsx
+++ src/app/components/CodeBlock.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, /*useState*/ } from "react";
 
 import { CodeBlock } from "@/components/ui/code-block";
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from '@/app/components/Navbar'
 import Footer from "@/app/components/Footer";
 import MobileWarning from "@/app/components/MobileWarning"
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Turbo Request – The Ultimate API Testing Tool",
@@ -27,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html className="dark" lang="en">
-      <body className={`${inter.variable} ${geistMono.variable} antialiased`}>
+      <body className={`antialiased`}>
         <main className='hidden md:block'>
           <Navbar />
           {children}
