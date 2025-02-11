ea11e10e1419057d16b62081b20d42a106ab6302
kartikmishra2004 <kartik26nov2004@gmail.com>
2025-02-12 00:25:03 +0530

    Migration to Auth.js 5

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,2 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth";
-import { NextRequest, NextResponse } from "next/server";
-
-const handler = NextAuth(authOptions);
-
-export function GET(req: NextRequest) {
-    return handler(req, NextResponse);
-}
-
-export function POST(req: NextRequest) {
-    return handler(req, NextResponse);
-}
+import { handlers } from "@/auth";
+export const { GET, POST } = handlers;
