d219ac97d14791cbd2c4913265f82ec95e8b32b1
kartikmishra2004 <kartik26nov2004@gmail.com>
2025-02-15 00:55:06 +0530

    loader

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function loading() {
+    return (
+        <div className="flex h-screen items-center justify-center">
+            <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-foreground"></div>
+        </div>
+    )
+}
