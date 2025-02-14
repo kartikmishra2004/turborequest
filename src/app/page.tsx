c0da8d00d87c4443990ba7513b59b48d7aa1884e
kartikmishra2004 <kartik26nov2004@gmail.com>
2025-02-15 01:43:17 +0530

    UX bug fix 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ import Hero from '@/app/components/Hero';
 import Features from '@/app/components/Features';
 import Screen from '@/app/components/Screen';
 import CTA from "@/app/components/CTA"
+import { auth } from '@/auth';
 
-export default function Home() {
-
+export default async function Home() {
+  await auth();
   return (
     <section className='min-h-screen'>
       <Hero />
