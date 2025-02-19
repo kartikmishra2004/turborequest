b0a72cc130cae45975b59bfbed2f251bbefdfc20
kartikmishra2004 <kartik26nov2004@gmail.com>
2025-02-19 11:58:42 +0530

    Create request API

---
+++ src/app/api/request/create/route.tsx
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import dbConnect from "@/lib/dbConnect";
+import User from "@/model/User.model";
+import { auth } from "@/auth";
+
+export async function POST(request: NextRequest) {
+    const session = await auth();
+    if (!session) {
+        return NextResponse.json({ error: "Unauthorized!!" }, { status: 401 });
+    }
+    try {
+        const { type, method, URL, headers, body, email, collectionName } = await request.json();
+        if (!type || !method || !URL || !headers || !body || !email || !collectionName) {
+            return NextResponse.json({ error: "Please enter the required fields!!" }, { status: 400 });
+        }
+        await dbConnect();
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            return NextResponse.json({ error: "User does not exist!!" }, { status: 404 });
+        }
+        const collection = user.collections.some(collection => collection.name === collectionName);
+        if (!collection) {
+            return NextResponse.json({ error: "Collection does not exist!!" }, { status: 404 });
+        }
+        await User.findOneAndUpdate({ email: email, "collections.name": collectionName }, { $push: { "collections.$.requests": { type: type, method: method, URL: URL, headers: headers, body: body } } }, { new: true });
+        return NextResponse.json({ message: "Request created successfully!!" }, { status: 200 });
+    } catch {
+        return NextResponse.json({ error: "Failed to create request!!" }, { status: 500 });
+    }
+}
